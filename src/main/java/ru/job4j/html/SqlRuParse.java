0741e75e9c5b486d2286e03d956c1f34f028e7ad
Александр <palenko.as@mail.ru>
2022-01-07 16:37:29 +0300

    2.4. SqlRuParse [#285213]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,24 +4,48 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.Post;
 import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SqlRuParse {
-    public static void main(String[] args) throws Exception {
-        String str = "https://www.sql.ru/forum/job-offers";
-        String plusStr = str;
-        for (int i = 0; i <= 5; i++) {
-            plusStr = str + "/" + i;
-        }
-        DateTimeParser dateTimeParser = new SqlRuDateTimeParser();
-        Document doc = Jsoup.connect(plusStr).get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element href = td.parent();
-            System.out.println(href.child(1).child(0).attr("href"));
-            System.out.println(href.child(1).text());
-            System.out.println(dateTimeParser.parse(href.child(5).text()));
-        }
+
+    private final DateTimeParser dateTimeParser;
+
+    public SqlRuParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
     }
+
+    /**
+     * Метод list загружает список постов
+     * @param link ссылка на страницу
+     *"@return список всех постов парсинга
+     */
+
+    /**
+     * Метод detail Загружает все детали одного поста (имя, описание, дату создания, ссылку на пост).
+     * @param link ссылка на страницу
+     * @return модель с данными.
+     */
+
+    List<Post> list(String link) throws IOException {
+        List<Post> post = new ArrayList<>();
+        Document doc = Jsoup.connect(link).get();
+        return post;
+    }
+
+    Post detail(String link) throws IOException {
+        Post post = new Post();
+        Document doc = Jsoup.connect(link).get();
+        Elements header = doc.select(".messageHeader");
+        Elements desc = doc.select(".msgBody");
+        Elements postCreated = doc.select(".msgFooter");
+        String href = doc.attr("href");
+        return post;
+    }
+
 }
