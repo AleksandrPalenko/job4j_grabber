706d47421ae3fd04b9cd7cda416438130afa0807
Александр <palenko.as@mail.ru>
2022-01-10 19:51:10 +0300

    5. PsqlStore [#285209]

---
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -0,0 +1,133 @@
+package ru.job4j.grabber;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
+import ru.job4j.html.SqlRuParse;
+import ru.job4j.quartz.AlertRabbit;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class PsqlStore implements Store, AutoCloseable {
+
+    private Connection cnn;
+
+    private static final Logger LOG = LoggerFactory.getLogger(AlertRabbit.class.getName());
+
+    public PsqlStore(Properties cfg) {
+        try {
+            Class.forName(cfg.getProperty("jdbc.driver"));
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        try {
+            cnn = DriverManager.getConnection(
+                    cfg.getProperty("url"),
+                    cfg.getProperty("username"),
+                    cfg.getProperty("password")
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Post postMethod(ResultSet rs) throws SQLException {
+        return new Post(
+                rs.getInt("id"),
+                rs.getString("name"),
+                rs.getString("text"),
+                rs.getString("link"),
+                rs.getTimestamp("text").toLocalDateTime()
+        );
+    }
+
+    @Override
+    public void save(Post post) {
+    /*
+    сохраняет объявление в базе
+     */
+        try (PreparedStatement statement =
+                     cnn.prepareStatement("Insert into past(name, text, link, created) values (?, ?, ?, ?)",
+                             Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, post.getTitle());
+            statement.setString(2, post.getDescription());
+            statement.setString(3, post.getLink());
+            statement.setString(4, post.getLink());
+            statement.setTimestamp(5, Timestamp.valueOf(post.getCreated()));
+            statement.execute();
+            try (ResultSet rs = statement.getGeneratedKeys()) {
+                if (rs.next()) {
+                    post.setId(rs.getInt("1"));
+                }
+            }
+        } catch (SQLException th) {
+            LOG.error(th.getMessage(), th);
+        }
+    }
+
+    @Override
+    public List<Post> getAll() {
+        /*
+        позволяет извлечь объявления из базы
+         */
+        List<Post> post = new ArrayList<>();
+        try (PreparedStatement pr = cnn.prepareStatement("select * from post")) {
+            try (ResultSet rs = pr.executeQuery()) {
+                while (rs.next()) {
+                    post.add(postMethod(rs));
+                }
+            }
+        } catch (SQLException th) {
+            LOG.error(th.getMessage(), th);
+        }
+        return post;
+    }
+
+    @Override
+    public Post findById(int id) {
+        /*
+        позволяет извлечь объявление из базы по id
+         */
+        Post postId = null;
+        try (PreparedStatement pr = cnn.prepareStatement("select * from post where id = ?")) {
+            pr.setInt(1, id);
+            try (ResultSet rs = pr.executeQuery()) {
+                while (rs.next()) {
+                    postId = postMethod(rs);
+                }
+            }
+        } catch (SQLException th) {
+            LOG.error(th.getMessage(), th);
+        }
+        return postId;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (cnn != null) {
+            cnn.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Properties config = new Properties();
+        try (InputStream in = PsqlStore.class.getClassLoader()
+                .getResourceAsStream("post.properties")) {
+            config.load(in);
+        } catch (Exception th) {
+            LOG.error(th.getMessage(), th);
+        }
+        PsqlStore ps = new PsqlStore(config);
+        SqlRuParse parse = new SqlRuParse(new SqlRuDateTimeParser());
+        String url = "https://www.sql.ru/forum/job-offers/";
+        List<Post> post = parse.list(url);
+        for (int i = 0; i < 5; i++) {
+            ps.save(post.get(i));
+        }
+    }
+}
--- src/main/java/ru/job4j/grabber/Store.java
+++ src/main/java/ru/job4j/grabber/Store.java
@@ -1,12 +1,13 @@
 package ru.job4j.grabber;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface Store {
     void save(Post post);
 
-    List<Post> getAll();
+    List<Post> getAll() throws SQLException;
 
-    Post findById(int id);
+    Post findById(int id) throws SQLException;
 
 }
