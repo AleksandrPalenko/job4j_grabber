ea5f2fdd9a011ed11e0ac4916a5039e21d0b5037
Александр <palenko.as@mail.ru>
2022-01-11 14:30:37 +0300

    6. Grabber. [#289477 #249343]

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -10,7 +10,7 @@ import ru.job4j.html.SqlRuParse;
 import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.newJob;
@@ -21,7 +21,7 @@ public class Grabber implements Grab {
     private final Properties cfg = new Properties();
 
     public Store store() {
-        return null;
+        return new PsqlStore(cfg);
     }
 
     public Scheduler scheduler() throws SchedulerException {
@@ -31,7 +31,7 @@ public class Grabber implements Grab {
     }
 
     public void cfg() throws IOException {
-        try (InputStream in = new FileInputStream(new File("app.properties"))) {
+        try (InputStream in = new FileInputStream(new File("src/main/resources/app.properties"))) {
             cfg.load(in);
         }
     }
@@ -46,9 +46,7 @@ public class Grabber implements Grab {
 
     @Override
     public void init(Parse parse, Store store, Scheduler scheduler) throws Exception {
-        try (Connection connection = connect(cfg)) {
             JobDataMap data = new JobDataMap();
-            data.put("connection", connection);
             data.put("store", store);
             data.put("parse", parse);
             JobDetail job = newJob(GrabJob.class)
@@ -62,7 +60,6 @@ public class Grabber implements Grab {
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        }
     }
 
     public static class GrabJob implements Job {
@@ -72,9 +69,13 @@ public class Grabber implements Grab {
             JobDataMap map = context.getJobDetail().getJobDataMap();
             Store store = (Store) map.get("store");
             Parse parse = (Parse) map.get("parse");
+            String url = "https://www.sql.ru/forum/job-offers/";
+            List<Post> post = parse.list(url);
+            for (int i = 0; i <= 5; i++) {
+                store.save(post.get(i));
+            }
         }
     }
-
 
     public static void main(String[] args) throws Exception {
         Grabber grab = new Grabber();
